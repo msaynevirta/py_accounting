8b28766da823c2bda95be384f3e5ce7a6fc9eea3
Markus Säynevirta <25539916+msaynevirta@users.noreply.github.com>
2020-05-08 16:55:51 +0300

    initial commit

---
+++ src/main.py
@@ -0,0 +1,46 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QGraphicsRectItem
+
+from py_gui.py_analytics.block_analytics import BlockAnalytics
+from py_gui.gui import Window
+
+class BlockGraphicsItem(QGraphicsRectItem):
+    '''
+    Handles drawing of blocks
+    '''
+
+class BlockDiagram(BlockGraphicsItem):
+    def __init__(self, expenses, income):
+        super(BlockDiagram, self).__init__()
+
+    def load_level(self, type, depth):
+        pass # loads data from certain level in income/expenses
+
+    def load_data(self):
+        # init with January
+        it = BlockAnalytics(
+            "/home/markus/repos/azure-accounting/src/data/sankey_config.json",
+            "/home/markus/repos/azure-accounting/src/data/main_database.json")
+        it.load_config()
+
+        for month in range(1, 13):
+            print("month: {:d}\n".format(month))
+            it.populate_config(month)
+
+        it.calculate_savings(0)
+
+def main():
+    # Every Qt application must have one instance of QApplication.
+    global APP # Use global to prevent crashing on exit
+    APP = QApplication(sys.argv)
+    window = Window(0,0,0)
+
+    window.show()
+
+    # Start the Qt event loop. (i.e. make it possible to interact with the gui)
+    sys.exit(APP.exec_())
+
+    # Any code below this point will only be executed after the gui is closed.
+
+if __name__ == '__main__':
+    main()
