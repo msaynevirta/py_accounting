0754c9528127152559cc533476fd11e821b9e5c0
Markus Säynevirta <markus.saynevirta@aalto.fi>
2020-06-03 01:00:23 +0300

    quarterly x-axis to cumulative line chart

--- src/py_analytics/line_graphics.py
+++ src/py_analytics/line_graphics.py
@@ -1,4 +1,4 @@
-from PyQt5.QtChart import QChart, QChartView
+from PyQt5.QtChart import QChart, QChartView, QValueAxis, QLineSeries
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter
@@ -7,27 +7,35 @@ class LineDiagram():
     def __init__(self, qseries_list, x_axis, title):
         self.qseries_list = qseries_list
         self.title = title
+
         self.x_axis = x_axis
+        self.y_axis = QValueAxis()
+        self.y_axis.setTickCount(6)
 
     def create_linechart(self):
         chart = QChart()
 
-        self.x_axis.setMin(0)
-        self.x_axis.setMax(365)
+        self.x_axis.setMin(1)
+        self.x_axis.setMax(366)
 
-        self.x_axis.append("Q1", 0)
-        self.x_axis.append("Q2", 91)
-        self.x_axis.append("Q3", 182)
-        self.x_axis.append("Q4", 274)
+        self.x_axis.append("Q1", 91)
+        self.x_axis.append("Q2", 182)
+        self.x_axis.append("Q3", 274)
+        self.x_axis.append("Q4", 365)
+
+        series = QLineSeries()
 
         for series in self.qseries_list:
-            #series.attachAxis(self.x_axis)
             chart.addSeries(series)
-            chart.setAxisX(self.x_axis, series)
 
         chart.createDefaultAxes()
+
+        chart.setAxisX(self.x_axis)
+
         chart.setAnimationOptions(QChart.SeriesAnimations)
         chart.setTitle(self.title)
+
+        
 
         chart.legend().setVisible(True)
         chart.legend().setAlignment(Qt.AlignBottom)
