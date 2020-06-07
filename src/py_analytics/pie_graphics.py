378ed2b87e5cc4303e9d183d4f71c6a16c4e7add
Markus Säynevirta <markus.saynevirta@aalto.fi>
2020-06-08 01:56:29 +0300

    Inherit QChart

--- src/py_analytics/pie_graphics.py
+++ src/py_analytics/pie_graphics.py
@@ -3,25 +3,20 @@ from PyQt5.QtChart import QChart, QChartView, QPieSlice
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QPen
 
-class PieDiagram():
+class PieDiagram(QChart):
     def __init__(self, qseries, legend, title):
+        super(PieDiagram, self).__init__()
         self.qseries = qseries
         self.legend = legend
         self.title = title
 
     def create_piechart(self):
         #adding pie chart
-        chart = QChart()
-        chart.legend().hide()
-        chart.addSeries(self.qseries)
-        chart.createDefaultAxes()
-        chart.setAnimationOptions(QChart.SeriesAnimations)
-        chart.setTitle("Payment methods")
+        self.legend().hide()
+        self.addSeries(self.qseries)
+        self.createDefaultAxes()
+        self.setAnimationOptions(QChart.SeriesAnimations)
+        self.setTitle("Payment methods")
 
-        chart.legend().setVisible(True)
-        chart.legend().setAlignment(Qt.AlignBottom)
- 
-        chartview = QChartView(chart)
-        chartview.setRenderHint(QPainter.Antialiasing)
-
-        return chartview
+        self.legend().setVisible(True)
+        self.legend().setAlignment(Qt.AlignBottom)
