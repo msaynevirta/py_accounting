1e0b1102b6b483fd63ed767e61146e9646b360c3
Markus Säynevirta <markus.saynevirta@aalto.fi>
2020-06-09 07:36:02 +0300

    Method for constructing two level tree hierarcies

--- src/py_analytics/block_analytics2.py
+++ src/py_analytics/block_analytics2.py
@@ -11,8 +11,9 @@ def json_loader(filedir): # collect data from main json database
         return False
 
 class BlockAnalytics():
+    """ Constructs two level tree hierarcy from totals collected from precollected data """
     def __init__(self, config_path, data_path, transaction_type, recipient_attribute_name, month):
-        self.links = []
+        self.tree = []
         self.config = json_loader(config_path) # block config from config.csv
         self.raw_json_data = json_loader(data_path) # main json database
 
@@ -20,24 +21,18 @@ class BlockAnalytics():
         self.recipient_attribute_name = recipient_attribute_name
         self.month = str(month)
 
-    def loop_links(self, looped_var):
-        name = None
-        sub_list = []
-        total = 0
-
-        for it in looped_var:
-            if it["sub"] is not None:
-                sub = self.loop_links(it["sub"])[2]
-                sub_list = sub[2]
-                total += sub[1]
-
+    def construct_tree(self, looped_var):
+        """ Construct two level tree hierarcy from config """
+        for level_1 in self.config:
+            if level_1["sub"] is not None:
+                sub_list = []
+                for level_2 in level_1["sub"]:
+                    sub_list.append([level_2["name"], self.collect_total_from_raw_data(level_2["path"]), None])
+                
+                self.tree.append([level_1["name"], sum(sub[1] for sub in sub_list), sub_list])
+            
             else:
-                # append to links!!!
-                name = it["name"]
-                total += self.collect_total_from_raw_data(it["path"])
-
-        return list(name, total, sub_list)
-
+                self.tree.append([level_1["name"], self.collect_total_from_raw_data(level_1["path"]), None])
 
     def collect_total_from_raw_data(self, path):
         main_cat = False
@@ -73,5 +68,37 @@ class BlockAnalytics():
         
         return total
 
-    def populate_config(self):
-        self.loop_links(self.config)
+    def collect_savings(self, month):
+        savings_in = 0.0
+        savings_out = 0.0
+
+        if month == 0: # collect all months
+            for month in range(1,13):
+                for it in self.config["savings"]["paths"]:
+                    savings_out += self.collect_total_from_raw_data(["expenses", "beneficiary"])
+                    savings_in += self.collect_total_from_raw_data(["income", "payer"])
+        else: # collect single month
+            for it in self.config["savings"]["paths"]:
+                savings_out += self.collect_total_from_raw_data(["expenses", "beneficiary"])
+                savings_in += self.collect_total_from_raw_data(["income", "payer"])
+
+        return savings_in, savings_out
+
+    def calculate_savings(self, month): 
+        """ Calculate transactions between savings accounts """
+        main_node = self.indexes.index(self.savings["main_node"])
+        savings_node = self.indexes.index(self.savings["savings_node"])
+
+        savings_in, savings_out = self.collect_savings(month)
+
+        if savings_in > savings_out and self.transaction_type == "income":
+            self.indexes.append("Säästötili")
+            self.config.append([self.indexes.index("Säästötili"), (savings_in - savings_out), main_node])
+
+        elif savings_in < savings_out and self.transaction_type == "expenses":
+            self.indexes.append("Säästötili")
+            self.config.append([savings_node, (savings_out - savings_in), self.indexes.index("Säästötili")])
+            
+            for it in self.config: # find savings node
+                if it[2] == savings_node:
+                    it[1] += (savings_out - savings_in) # add remaining savings to the savings node
