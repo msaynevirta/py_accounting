3f9c3b81feb4a1ae7656159df98dfab4d9a8757a
Markus Säynevirta <markus.saynevirta@aalto.fi>
2020-06-09 03:58:27 +0300

    Use pre-existing transaction data

--- src/py_analytics/payment_methods.py
+++ src/py_analytics/payment_methods.py
@@ -3,32 +3,22 @@ from json import load, JSONDecodeError
 from PyQt5.QtChart import QPieSeries
 from PyQt5.QtCore import pyqtSignal, QEvent
 
-def json_loader(filedir): # collect data from main json database
-    try:
-        with open(filedir, 'r', encoding='utf-8') as data_file:
-            data = load(data_file)
-            return data
-
-    except JSONDecodeError:
-        print('\033[31m' + "Error: " + '\033[0m' + "Failed to read the data file")
-        return False
-
 class PaymentMethods(QPieSeries):
     '''
     Prepares pie chart of payment methods for plotting with PyQtCharts (supports currently only expenses)
     '''
-    def __init__(self, data_path):
+    def __init__(self, data, year):
         super(PaymentMethods, self).__init__()
-        self.raw_json_data = json_loader(data_path) # main json database
-        
         series = {}
 
         for month in range(1,13):
             for transaction_type in ["expenses", "income"]: # go through both by month
-                for it in self.raw_json_data[transaction_type]["months"][str(month)]:
-
-                    main_key = it["payment_method"][0][0]
-                    sub_key = it["payment_method"][0][1]
+                for it in data[str(year)][transaction_type]["months"][str(month)]:
+                    try:
+                        main_key = it["payment_method"][0][0]
+                        sub_key = it["payment_method"][0][1]
+                    except IndexError:
+                        continue
 
                     try:
                         series[main_key]
