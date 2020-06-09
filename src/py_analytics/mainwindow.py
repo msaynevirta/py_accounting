40e1edc01a31db0533f86f310534e7a4003f9952
Markus Säynevirta <markus.saynevirta@aalto.fi>
2020-06-09 03:57:40 +0300

    Move chart creation to a separate method, add support for pie method chart

--- src/py_analytics/mainwindow.py
+++ src/py_analytics/mainwindow.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtChart import QChart
+from PyQt5.QtCore import Qt
 
 from ui_mainwindow import Ui_MainWindow
 
@@ -27,12 +29,19 @@ class MainWindow:
         self.main_window.show()
 
     def load_transactions(self):
-        self.file_paths, _ = QFileDialog.getOpenFileNames(self.ui.stackedWidget, "Choose transaction files", "/home", "JSON files (*.json)")
+        self.file_paths, _ = QFileDialog.getOpenFileNames(self.ui.stackedWidget,
+            "Choose transaction files", "/home", "JSON files (*.json)")
         self.data = TransActionLoader(self.file_paths).load_transactions()
 
         self.build_analytics()
 
     def build_analytics(self):
+        self.cumulative_expenses_line()
+        self.method_pie(max(self.data)) # Create chart for most recent year
+
+        self.ui.stackedWidget.setCurrentIndex(1) # Switch to analytics
+
+    def cumulative_expenses_line(self):
         two_largest_keys = [int(it) for it in sorted(self.data.keys(), reverse=True)[:2]]
 
         qlineseries_expenses = []
@@ -40,10 +49,20 @@ class MainWindow:
         for year in two_largest_keys:
             qlineseries_expenses.append(YearLineAnalytics(self.data, year))
 
-        #self.ui.v_block_diagram.setChart()
         self.ui.v_cumulative_expenses.setChart(QuarterlyLineDiagram(qlineseries_expenses, "Cumulative expenses"))
 
-        self.ui.stackedWidget.setCurrentIndex(1) # Switch to analytics
+    def method_pie(self, year):
+        pie_chart = QChart()
+        pie_chart.legend().hide()
+        pie_chart.addSeries(PaymentMethods(self.data, year))
+        pie_chart.createDefaultAxes()
+        pie_chart.setAnimationOptions(QChart.SeriesAnimations)
+        pie_chart.setTitle("Payment methods")
+
+        pie_chart.legend().setVisible(True)
+        pie_chart.legend().setAlignment(Qt.AlignBottom)
+
+        self.ui.v_method_pie.setChart(pie_chart)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
